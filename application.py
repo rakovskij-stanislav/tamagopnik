2bf308614b097677911597451580046c9a3dda56
rakovskij_stanislav <rakovskij.stanislav@gmail.com>
2017-01-25 23:04:30 +0300

    Вжух ~ и явился великий PEP-8! Также попробовал логгирование онлайна

--- application.py
+++ application.py
@@ -1,30 +1,32 @@
 # гитхаб создателя библиотеки - https://github.com/eternnoir/pyTelegramBotAPI
 import telebot
 from telebot import types
-import extra # тут хранятся тексты
-import db # тут хранятся пользователи
-import _thread #задел на будущее
+import extra  # тут хранятся тексты
+import db  # тут хранятся пользователи
+import _thread  # задел на будущее
 import random
-import config # тут хранится логин-пароль от бота
+import config  # тут хранится логин-пароль от бота
 import time
+from datetime import datetime
 
 __author__ = "Rakovskij Stanislav"
-__ver__    = "0.1"
+__ver__ = "0.1"
 admin_id = 287004218
 
-### импортируем базу данных игроков
+# импортируем базу данных игроков
 player_base = db.db()
 
-### инициализация бота
+# инициализация бота
 bot = telebot.TeleBot(config.telebot[0])
+
 
 @bot.message_handler(commands=['admin'])
 def admin_panel(mes):
-    if mes.chat.id != admin_id :
+    if mes.chat.id != admin_id:
         return ''
     com = str(mes.text).split()
-    if len(com)>1:
-        if com[1] in ['load', 'merge','save','wipe', "online"]:
+    if len(com) > 1:
+        if com[1] in ['load', 'merge', 'save', 'wipe', "online"]:
             if com[1] == 'load':
                 player_base.load()
             if com[1] == 'merge':
@@ -37,19 +39,18 @@ def admin_panel(mes):
                 i = 0
                 t = time.time()
                 for q in player_base.base.keys():
-                    if t - player_base.base[q].last_activity < 60*5:
-                        i+=1
-                bot.send_message(mes.chat.id, "Online users : "+str(i))
-
+                    if t - player_base.base[q].last_activity < 60 * 5:
+                        i += 1
+                bot.send_message(mes.chat.id, "Online users : " + str(i))
 
 
 # действия, которые надо применить при комманде start. Она может играть роль принудительного начала новой игры.
 @bot.message_handler(commands=['start'])
 def send_welcome(mes):
-    #print(mes)
+    # print(mes)
     if mes.chat.id in player_base.base.keys():
-        #если старый игрок требует старт
-        player_base.base[mes.chat.id].recovery=True
+        # если старый игрок требует старт
+        player_base.base[mes.chat.id].recovery = True
         markup = types.ReplyKeyboardMarkup()
         it_a = types.KeyboardButton('/new Начать новую игру')
         it_b = types.KeyboardButton('/con Продолжить прошлую игру')
@@ -60,24 +61,27 @@ def send_welcome(mes):
         ###что делать, если игрок действительно новый
         player = db.player()
         player.last_motion = ['startroom', 'quest', 0]
-        player_base.base.update({mes.chat.id:player})
-        send_message(mes) #основная функция по обработке сообщений
+        player_base.base.update({mes.chat.id: player})
+        send_message(mes)  # основная функция по обработке сообщений
+
 
 @bot.message_handler(commands=['help'])
 def send_help(mes):
     bot.send_message(mes.chat.id, 'Понимаю, тут должен быль хелп-гайд, но его нет пока')
 
+
 @bot.message_handler(commands=['new'])
-def new_start(mes): #служебная комманда для быстрого удаления информации о текущем пользователе
+def new_start(mes):  # служебная комманда для быстрого удаления информации о текущем пользователе
     if mes.chat.id in player_base.base:
         player_base.base.pop(mes.chat.id)
     print("InfoNEW :", mes.chat.id)
     send_message(mes)
     return ''
 
+
 @bot.message_handler(commands=['con'])
-def new_cont(mes): #продолжить игру. надо снять флаг ожидания выбора
-    if mes.chat.id not in player_base.base: #хехе, если кто-то прошмыгнул мимо /start
+def new_cont(mes):  # продолжить игру. надо снять флаг ожидания выбора
+    if mes.chat.id not in player_base.base:  # хехе, если кто-то прошмыгнул мимо /start
         send_welcome(mes)
         return ''
     print("InfoCON :", mes.chat.id)
@@ -89,48 +93,49 @@ def new_cont(mes): #продолжить игру. надо снять флаг 
 
 @bot.message_handler()
 def send_message(mes):
-    #Основная часть движка
-    if mes.chat.id not in player_base.base: #хехе, если кто-то прошмыгнул мимо /start
+    # Основная часть движка
+    if mes.chat.id not in player_base.base:  # хехе, если кто-то прошмыгнул мимо /start
         send_welcome(mes)
         return ''
     player = player_base.base[mes.chat.id]
     player.recovery = False
     player.turns += 1
     player.last_activity = time.time()
-    #print("extra."+player.last_motion[0])
-    local_room = eval("extra."+player.last_motion[0]+'()', {"player":player, "extra":extra}) #получаем информацию о комнате
+    # print("extra."+player.last_motion[0])
+    local_room = eval("extra." + player.last_motion[0] + '()',
+                      {"player": player, "extra": extra})  # получаем информацию о комнате
     dialog = eval('local_room.' + player.last_motion[1])
     pos = player.last_motion[2]
     message = str(mes.text)
-    #print('Info :', local_room, dialog, pos, mes)
-    #print("Info2 :", str(type(dialog[pos])))
-    #print("Info3 :", str(dialog[pos]))
-    #print('Info :', mes.chat.id, mes.text)
-    if str(type(dialog[pos])) == "<class 'str'>": #для обычных текстов
-        player.last_motion[2]+=1
-        #player_base.base[mes.chat.id] = player
+    # print('Info :', local_room, dialog, pos, mes)
+    # print("Info2 :", str(type(dialog[pos])))
+    # print("Info3 :", str(dialog[pos]))
+    # print('Info :', mes.chat.id, mes.text)
+    if str(type(dialog[pos])) == "<class 'str'>":  # для обычных текстов
+        player.last_motion[2] += 1
+        # player_base.base[mes.chat.id] = player
         print('Info0 :', mes.chat.id, 'Прошлое сообщение человека :', mes.text, '\n   Отвечаю ему', dialog[pos])
         bot.send_message(mes.chat.id, dialog[pos], parse_mode="html")
         send_message(mes)
         return ''
     else:
-        #need execute motions:
+        # need execute motions:
         motion = dialog[pos]
         if motion[0] == "choose":
             motto = motion[1]
             varss = motto.keys()
             text = '%Твой выбор:%'
-            if message in varss: #если игрок прислал ответ
-                #sender(mes, "ti molode4", markup)
-                player.last_motion[2]=0
+            if message in varss:  # если игрок прислал ответ
+                # sender(mes, "ti molode4", markup)
+                player.last_motion[2] = 0
                 player.last_motion[1] = motto[message]
                 player.waiting_answer = False
                 send_message(mes)
                 return ''
-            else: #если мы лишь перешли на этот блок или надо повторить отправку
-                #sender(mes, "tebe otdam otvet", markup)
-                if len(motion)==3:
-                    text=motion[2]
+            else:  # если мы лишь перешли на этот блок или надо повторить отправку
+                # sender(mes, "tebe otdam otvet", markup)
+                if len(motion) == 3:
+                    text = motion[2]
                 markup = types.ReplyKeyboardMarkup()
                 for i in varss:
                     markup.row(types.KeyboardButton(i))
@@ -157,9 +162,9 @@ def send_message(mes):
 
         elif motion[0] == "take":
             player.inventory.append(motion[1])
-            player.last_motion[2]+=1
+            player.last_motion[2] += 1
             player_base.base[mes.chat.id] = player
-            if len(motion)==3:
+            if len(motion) == 3:
                 print('Info3 :', mes.chat.id, mes.text)
                 bot.send_message(mes.chat.id, motion[2], parse_mode="html")
             send_message(mes)
@@ -168,13 +173,15 @@ def send_message(mes):
         elif motion[0] == "drop":
             if motion[1] not in player.inventory:
                 print('Info4 :', mes.chat.id, mes.text)
-                bot.send_message(mes.chat.id, "Обнаружена нецелостность квеста. Сообщите об этом автору.\n Техническая информация :"+str(local_room)+'/'+str(dialog)+'/'+str(pos))
-                print('ERROR 9', mes.chat.id,local_room, dialog, pos, mes)
+                bot.send_message(mes.chat.id,
+                                 "Обнаружена нецелостность квеста. Сообщите об этом автору.\n Техническая информация :" + str(
+                                     local_room) + '/' + str(dialog) + '/' + str(pos))
+                print('ERROR 9', mes.chat.id, local_room, dialog, pos, mes)
                 return ''
             del player.inventory[player.inventory.index(motion[1])]
-            player.last_motion[2]+=1
+            player.last_motion[2] += 1
             player_base.base[mes.chat.id] = player
-            if len(motion)==3:
+            if len(motion) == 3:
                 print('Info5 :', mes.chat.id, mes.text)
                 bot.send_message(mes.chat.id, motion[2], parse_mode="html")
             else:
@@ -194,32 +201,30 @@ def send_message(mes):
             return ''
 
         elif motion[0] == "random":
-            ran = len(motion)-1
+            ran = len(motion) - 1
             ans = random.randrange(ran)
-            player.last_motion[1] = motion[1+ans]
+            player.last_motion[1] = motion[1 + ans]
             player.last_motion[2] = 0
             send_message(mes)
             return ''
         elif motion[0] == "compile":
             ans = motion[1]
-            ans = eval('"'+ans+'".format('+motion[2]+')')
-            player.last_motion[2]+=1
+            ans = eval('"' + ans + '".format(' + motion[2] + ')')
+            player.last_motion[2] += 1
             print('Info7 :', mes.chat.id, mes.text)
             bot.send_message(mes.chat.id, ans, parse_mode="html")
             send_message(mes)
             return ""
 
         elif motion[0] == "reset_but":
-            player.last_motion[2]+=1
+            player.last_motion[2] += 1
             print('Info8 :', mes.chat.id, mes.text)
             markup = types.ReplyKeyboardHide(selective=False)
             bot.send_message(mes.chat.id, motion[1], parse_mode="html", reply_markup=markup)
             send_message(mes)
             return ''
 
-
         bot.send_message(mes.chat.id, 'Еще надо подумать', parse_mode="html")
-
 
 
 """
@@ -241,7 +246,31 @@ def send(mes):
     bot.send_message(mes.chat.id, mes.text, reply_markup=markup)
 """
 
-bot_thread = _thread.start_new(bot.polling, ())
-a = input("Enter exit if it is IDLE")
-### Работаю в PyCharm, поэтому нужна точка остановки
 
+def metrika():
+    # Тому, кто всё-таки отважился читать мой код
+    # Я тут подумал, можно в Гугл.Формах хранить логи. Но законно ли это? Я же почти раз в минуту пишу логи, не забанят ли за ~1440 обращений с одного ip?
+    # Пожалуйста, ответьте, интересно ведь
+    name = str(datetime.now())[:-7]
+    with open('metrika/'+name + '.csv', "w") as f:
+        f.write(name + ';0\n')
+    time.sleep(60)
+    while True:
+        with open('metrika/'+name + '.csv', "a") as f:
+            t = time.time()
+            i = 0
+            for player in player_base.base.keys():
+                if player_base.base[player].last_activity - t < 60 * 5:
+                    i += 1
+            f.write(str(datetime.now())[:-7] + ';' + str(i) + '\n')
+        print("#Logged")
+        time.sleep(60)
+
+
+# Единственная метрика на данный момент - онлайн, сохранять буду в формате csv
+metr = input("Введите '1', если хотите сохранять метрику : ")
+if metr == "1":
+    now = datetime.now()
+    metr_thread = _thread.start_new(metrika, ())
+
+bot.polling()
--- db.py
+++ db.py
@@ -1,29 +1,32 @@
 import pickle
 
+
 class db():
     def __init__(self):
-        self.base = {} #id - экземпляр player
+        self.base = {}  # id - экземпляр player
         self.path = "backup.db"
+
     def load(self, path=""):
         if path == "": path = self.path
         base_backup = dict(self.base)
         try:
             with open(path) as f:
                 self.base = pickle.load(f)
-            if str(type(self.base))!="<class 'dict'>":
+            if str(type(self.base)) != "<class 'dict'>":
                 raise Exception("NON_DICT")
         except Exception as e:
             self.base = base_backup()
             print("Error while load a backup :", str(e))
         else:
             print("loaded")
+
     def merge(self, path=''):
         if path == "": path = self.path
         base_backup = dict(self.base)
         try:
             with open(path) as f:
                 basem = pickle.load(f)
-            if str(type(basem))!="<class 'dict'>":
+            if str(type(basem)) != "<class 'dict'>":
                 raise Exception("NON_DICT_basem")
             self.base.update(basem)
         except Exception as e:
@@ -31,20 +34,23 @@ class db():
             print("Error while load a backup :", str(e))
         else:
             print("Merged")
+
     def save(self, path=''):
         if path == "": path = self.path
         with open(path, "wb") as f:
             pickle.dump([self.base], f)
         print("Saved")
+
     def wipe(self):
         self.base = {}
         print("Wiped")
 
+
 class player():
     def __init__(self):
-        self.last_motion = [] #[room,  dialog,  position_in_dialog]
+        self.last_motion = []  # [room,  dialog,  position_in_dialog]
         self.inventory = []
-        self.recovery = False #режим восстановления учетной записи
+        self.recovery = False  # режим восстановления учетной записи
         self.turns = 0
         self.last_activity = 0
         self.waiting_answer = False
